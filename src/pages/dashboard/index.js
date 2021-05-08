c1d8298385312b934ee4421bacb1675dd9cab86a
kkcy <rickykkcy@gmail.com>
2021-05-08 23:56:35 +0800

    add dashboard page

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,11 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { DateTime } from 'luxon'
+import { useSnapshot } from 'valtio'
+
+import { Box, Typography } from '@material-ui/core'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemText from '@material-ui/core/ListItemText'
+import Divider from '@material-ui/core/Divider'
+
+import store from '../../store/auth'
 
 const DashboardPage = () => {
-    return (
-        <div>
-            
-        </div>
-    )
+  const { profile } = useSnapshot(store)
+  const [today] = useState(DateTime.local())
+
+  return (
+    <>
+      <Box display="flex" justifyContent="space-between">
+        <Typography variant="h3">Welcome Back {profile.username}</Typography>
+        <Typography variant="h4">{today.toJSDate().toDateString()}</Typography>
+      </Box>
+
+      <Box p={2} />
+
+      <Box>
+        <Typography variant="h6">You may...</Typography>
+        <List>
+          <ListItem button>
+            <ListItemText>Create a new harta</ListItemText>
+          </ListItem>
+          <Divider />
+          <ListItem button>
+            <ListItemText>Look for owners with Carian</ListItemText>
+          </ListItem>
+        </List>
+      </Box>
+    </>
+  )
 }
 
 export default DashboardPage
